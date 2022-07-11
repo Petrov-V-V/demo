0ad9eab59354e3e9ed4d62391b3d6087d11e48e0
Petrov-V-V <vp6514764@gmail.com>
2022-07-11 09:21:29 +0300

    Format changes

--- src/main/java/com/example/greek_capital_phi/Program.java
+++ src/main/java/com/example/greek_capital_phi/Program.java
@@ -10,21 +10,20 @@ import java.util.function.Function;
 public class Program {
 	public static double listTask() {
 		List<Integer> integerList = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-		return integerList.stream().limit(integerList.size() - 3)
-				.mapToInt(a -> a).map(a -> a % 2 == 0 ? a + 5 : a - 5).average().getAsDouble();
+		return integerList.stream().limit(integerList.size() - 3).mapToInt(a -> a).map(a -> a % 2 == 0 ? a + 5 : a - 5)
+				.average().getAsDouble();
 	}
 
 	public static Map<String, String> setTask() {
-		Set<String> stringSet = Set.of("zero", "one", "two", "three", "four",
-				"twenty-three", "thirty-four", "seventy-seven", "eighty-eight", "one hundred");
+		Set<String> stringSet = Set.of("zero", "one", "two", "three", "four", "twenty-three", "thirty-four",
+				"seventy-seven", "eighty-eight", "one hundred");
 		return stringSet.stream().filter(a -> a.length() < 10)
 				.collect(Collectors.toMap(Function.identity(), Function.identity()));
 	}
 
 	public static List<String> mapFirstTask() {
 		Map<String, String> stringMap = setTask();
-		return stringMap.entrySet().stream().map(a -> a.getKey() + " == " + a.getValue())
-				.collect(Collectors.toList());
+		return stringMap.entrySet().stream().map(a -> a.getKey() + " == " + a.getValue()).collect(Collectors.toList());
 	}
 
 	public static List<String> mapSecondTask() {
