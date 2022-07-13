f6b28308790a85eff77e4211e829f7ccf1777a62
Petrov-V-V <vp6514764@gmail.com>
2022-07-13 10:37:08 +0300

    Multithreading

---
+++ src/main/java/com/example/multithreading/Resources.java
@@ -0,0 +1,21 @@
+package com.example.multithreading;
+
+public class Resources {
+    private volatile int sharedResource = 0;
+
+    public synchronized void addResource() throws Exception {
+        if (sharedResource > 7)
+            wait();
+        sharedResource++;
+        System.out.println(sharedResource);
+        notify();
+    }
+
+    public synchronized void removeResource() throws Exception {
+        if (sharedResource <= 0)
+            wait();
+        sharedResource--;
+        System.out.println(sharedResource);
+        notify();
+    }
+}
---
+++ src/test/java/com/example/multithreading/MultithreadingTest.java
@@ -0,0 +1,91 @@
+package com.example.multithreading;
+
+import org.junit.Test;
+
+public class MultithreadingTest {
+    @Test
+    public void firstTaskTest() {
+        Thread thread = new NamePrintThread();
+        Thread runnable = new Thread(new NamePrintRunnable());
+        thread.start();
+        runnable.start();
+    }
+
+    @Test
+    public void secondTaskTest() throws Exception {
+        Thread thread1 = new Thread(() -> System.out.println("thread"));
+        Thread thread2 = new Thread(() -> System.out.println("main thread"));
+        thread1.start();
+        thread1.join();
+        thread2.start();
+    }
+
+    @Test
+    public void thirdTaskTest() {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+
+    @Test
+    public void fourthTaskTest() {
+        Resources resource = new Resources();
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 8; i++)
+                try {
+                    resource.addResource();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        });
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0; i < 8; i++)
+                try {
+                    resource.removeResource();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        });
+        thread1.start();
+        thread2.start();
+    }
+
+    @Test
+    public void fifthTaskTest() throws Exception {
+        Object lock = new Object();
+        Thread thread = new Thread(() -> {
+            try {
+                while (true) {
+                    Thread.sleep(5000);
+                    synchronized (lock) {
+                        System.out.println("Print smth");
+                        lock.notify();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
+
+    @Test
+    public void sixthTaskTest() throws Exception {
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {}
+        });
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
+    }
+}
